d352205f9da80588e8cc95aaac8e7e45c97b82b9
reinel215 <44073967+reinel215@users.noreply.github.com>
2018-12-11 16:38:20 -0400

    auth y logout
    cree la autenticacion para usuarios no logeados y un logout

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,18 +2,20 @@ var express = require('express');
 var router = express.Router();
 var passport= require('passport');
 var controllers = require('.././controllers');
-//var AuthMiddleware = require('.././middleware/auth');
+var AuthMiddleware = require('.././middleware/auth');
 
+
+// "GET" requests
 router.get('/auth/logout',controllers.UserController.logout);
-router.post('/auth/signup',controllers.UserController.postSignUp);
-router.post('/auth/signin',passport.authenticate('local',{
+router.get('/users/panel', AuthMiddleware.isLogged ,controllers.UserController.getUserPanel);
+
+
+//"POST" request
+router.post('/auth/signup',controllers.UserController.postSignUp);//registrar
+router.post('/auth/signin',passport.authenticate('local',{//iniciar sesion
 	successRedirect : '/perfil',
 	failureRedirect : '/',
 }));
 
-//rutas de usuario
-/*
-router.get('/auth/logout',controllers.UserController.logout);
-router.get('/users/panel',AuthMiddleware.isLogged,controllers.UserController.getUserPanel);
-*/
+
 module.exports = router;
